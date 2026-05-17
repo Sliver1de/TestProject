2c034a26ca63398349ea91323218f7aad137a987
Sliver1de <xinweifeng8@gmail.com>
2026-05-17 17:10:19 +0800

    增加代码

---
+++ Test.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Test;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("测试用代码");
+
+        List<int> list = new List<int>();
+    }
+}
